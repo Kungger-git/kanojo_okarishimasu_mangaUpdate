d311cd936bf982144fb6e260dae5d08a49988ea8
KungPaoChick <matthewriel63@gmail.com>
2020-12-26 11:39:34 +0800

    Finished it

--- Kanojo_manga_update_sel.py
+++ Kanojo_manga_update_sel.py
@@ -3,27 +3,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import colorama, time
+import colorama
 
 
 def main(driver):
     driver.get(
         'https://mangajar.com/manga/kanojo-okarishimasu')
-    try:
-        getTitle = WebDriverWait(driver, 0).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '/html/body/div[1]/div/article[1]/div/h1/span[1]'))
-        )
-    finally:
-        pass
-    time.sleep(1)
-    driver.quit()
 
-    print(getTitle)
-    
+    try:
+        article = WebDriverWait(driver, 0).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '/html/body/div[1]/div/article[1]'))
+        )
+        for tag in article.find_elements_by_tag_name('h1'):
+            title = tag.find_element_by_class_name('post-name')
+            print('\n')
+            print(colorama.Fore.LIGHTCYAN_EX, title.text, colorama.Style.RESET_ALL)
+
+        for chapter in article.find_elements_by_xpath('/html/body/div[1]/div/article[1]/div/div[2]/a[2]'):
+            new = chapter.find_element_by_class_name('h-6')
+            time = driver.find_element_by_xpath('/html/body/div[1]/div/article[2]/ul/li[1]/span')
+            print('\nNew Chapter:')
+            print(colorama.Fore.GREEN, new.text.replace('Read', 'Chapter'), colorama.Style.RESET_ALL, time.text)
+    finally:
+        driver.quit()
+
 
 if __name__ == '__main__':
     edge = webdriver.Edge(
         '/Users/jimfe/Documents/msedgedriver.exe')
     colorama.init()
     main(edge)
+    print('\n\n')
